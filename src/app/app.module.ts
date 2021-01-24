d32c028580ef0fd2b9c0c614d1851a747f279a9e
digamber-dwivedi-au9 <digamdip@gmail.com>
2021-01-25 00:27:17 +0530

    product add and services model and class generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import { NavComponent } from './components/shared/nav/nav.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
 import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
-import { CartComponent } from './components/shopping-cart/cart/cart.component';
-import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
+import { CartComponent } from '../../../cart/cart.component';
+import { CartItemComponent } from '../../../cart/cart-item/cart-item.component';
 import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
 
 @NgModule({
